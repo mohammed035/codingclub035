3ed7ea5a805dccb51c8aedea7d8dfba8eb30fe65
mohammed035 <42632631+mohammed035@users.noreply.github.com>
2020-08-08 15:37:23 +0530

    Delete p2.sh

--- p2.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -x
-read -p "enter a number" n
-harmonic=0;
-for((i=1; i<=$n; i++))
-do
-a= `echo 1 $n | awk '{print $1/$2}'`
-harmonic=`echo $harmonic $a | awk '{print $1+$2}'`
-done
-
