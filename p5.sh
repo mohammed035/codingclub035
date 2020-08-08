45af262d8de70eb36834b99f7d8b873a75fa4456
mohammed035 <42632631+mohammed035@users.noreply.github.com>
2020-08-08 15:38:01 +0530

    Delete p5.sh

--- p5.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -x 
-read -p "Enter a number " number
-factorial=1
-for(( i=1; i<=number; i++ ))
-do
-  factorial=$[ $factorial * $i ]
-done
-echo "The factorial of $number is $factorial"
